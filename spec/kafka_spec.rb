b31b171aaa03ebbd77cb4dc59ace49c2466a5c5f
Mubashir Kamran <mubashir.kamran@LTMY-0046-DEBE.local>
2019-12-06 15:29:22 +0100

    test cases completed and unused spec files removed

--- spec/kafka_spec.rb
+++ spec/kafka_spec.rb
@@ -22,48 +22,58 @@ describe 'kafka_application' do
     let(:fake_avro_client) { double("Avro") }
     let(:fake_kafka_producer) { double("KafkaProducer") }
     let(:fake_kafka_consumer) { double("KafkaConsumer") }
-
     let(:hobby_name) { "swimming" }
+    let(:fake_kafka_message) {
+    }
 
 
     context "testing the producer's write and delivering functionalities" do
-    before do
-      allow_any_instance_of(KafkaApp).to receive(:kafka_client).and_return(fake_kafka_client)
-      allow_any_instance_of(KafkaProducer).to receive(:avro_client).and_return(fake_avro_client)
-      allow_any_instance_of(KafkaProducer).to receive(:producer).and_return(fake_kafka_producer)
-      allow(fake_avro_client).to receive(:encode).with({"title" => hobby_name, "user" => "mubashir"}, schema_name: avro_schema)
+      before do
+        allow_any_instance_of(KafkaApp).to receive(:kafka_client).and_return(fake_kafka_client)
+        allow_any_instance_of(KafkaProducer).to receive(:avro_client).and_return(fake_avro_client)
+        allow_any_instance_of(KafkaProducer).to receive(:producer).and_return(fake_kafka_producer)
+        allow(fake_avro_client).to receive(:encode).with({"title" => hobby_name, "user" => "mubashir"},
+                                                         schema_name: avro_schema)
+      end
+
+      it "client should be able to successfully create the topic" do
+        expect(fake_kafka_client).to receive(:create_topic).with(topic, num_partitions: num_of_partitions,
+                                                                 replication_factor: replication_per_partition)
+        KafkaApp.new.create_topic(topic, num_of_partitions, replication_per_partition)
+      end
+
+      it "should be able to successfully write and deliver messages to topic" do
+        expect(fake_kafka_producer).to receive(:produce).with(anything, topic: topic)
+        expect(fake_kafka_producer).to receive(:deliver_messages)
+
+        kafka_producer.write_message(hobby_name, topic, avro_schema)
+        kafka_producer.deliver_all_messages
+      end
     end
-
-    it "client should be able to successfully create the topic" do
-
-      expect(fake_kafka_client).to receive(:create_topic).with(topic, num_partitions: num_of_partitions, replication_factor: replication_per_partition)
-      KafkaApp.new.create_topic(topic, num_of_partitions, replication_per_partition)
-    end
-
-    it "should be able to successfully write and deliver messages to topic" do
-      expect(fake_kafka_producer).to receive(:produce).with(anything, topic: topic)
-      expect(fake_kafka_producer).to receive(:deliver_messages)
-
-      kafka_producer.write_message(hobby_name, topic, avro_schema)
-      kafka_producer.deliver_all_messages
-    end
-  end
 
 
   context "testing the consumer's subscribe functionality" do
     before do
       allow_any_instance_of(KafkaConsumer).to receive(:consumer).with(topic).and_return(fake_kafka_consumer)
+      allow_any_instance_of(KafkaConsumer).to receive(:avro_client).and_return(fake_avro_client)
 
-
+      allow(fake_kafka_consumer).to receive(:each_message).and_yield(fake_kafka_message)
+      allow(fake_avro_client).to receive(:decode).with(anything, schema_name: avro_schema)
     end
 
     it "should be able to successfully subscribe to topic and then consuming messages from topic" do
       expect(fake_kafka_consumer).to receive(:subscribe).with(topic, start_from_beginning: true)
-      allow(fake_kafka_consumer).to receive(:each_message)
-
       kafka_consumer.subscribe_to_topic(topic, true)
+    end
+
+    it "should be able to succesfully consume messages from topic" do
+      expect(fake_kafka_message).to receive(:offset).and_return(1)
+      expect(fake_kafka_message).to receive(:partition).and_return(1)
+      expect(fake_kafka_message).to receive(:value).and_return("some value").at_most(2).times
+
       kafka_consumer.consume_messages(topic, avro_schema)
     end
+
   end
 
 
